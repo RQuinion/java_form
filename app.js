2a68abb96e053c2daa2e9f4fed95af0f5e4e906c
romain <r.quinion@gmail.com>
2023-01-13 09:55:55 +0100

    correction de value

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ contactForm.addEventListener('submit',(event) => {
     passwordError.style.display = 'none';
 
     if (!formData.name || !formData.phone || !formData.email || !formData.password) {
-        const nameRegex = /^[a-z0-9]{2,50}$/i;
-        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm
-        const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
+        const nameRegex = /^[a-zA-Z ]+$/;
+        const emailRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
+        const phoneRegex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/;
 
         if (!formData.name || !nameRegex.test(formData.name)) {
             errors.name = true;
